f89dd36230ad27f0ad6a25bab8eab570b1595b2f
Chris Kelley <ckelley@redhat.com>
2021-10-21 15:34:10 +0100

    Check ACME server is ready before polling it in ACME CI tests
    Potentially fixes intermittent CI failures due to a race condition
    starting up the ACME server. We'll have to merge and see if the failures
    stop. Currently we see it 28% of the time (7 fails in last 25 runs)

---
+++ tests/bin/check-acme-server-ready.sh
@@ -0,0 +1,28 @@
+echo "Waiting for ACME server to be ready"
+start_time=$(date +%s)
+MAX_WAIT=60
+
+while :
+do
+    sleep 1
+
+    response=$(curl -k --write-out '%{http_code}' --silent --output /dev/null http://pki.example.com:8080/acme/directory)
+    echo ${response}
+    if [ ${response} == "200" ]
+    then
+        break
+    fi
+
+    current_time=$(date +%s)
+    elapsed_time=$(expr $current_time - $start_time)
+
+    if [ $elapsed_time -ge $MAX_WAIT ]
+    then
+        echo "ACME server not ready after ${MAX_WAIT}s"
+        exit 1
+    fi
+
+    echo "Waiting for ACME server to start (${elapsed_time}s)"
+done
+
+echo "ACME server is started"
